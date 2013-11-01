2c6b6e94f8a5894b9702a0787e93368a342b3b84
Adrien Brault <adrien.brault@gmail.com>
2013-10-31 22:10:07 -0700

    Add request method/route to SymfonyRequestProvider

--- src/AdrienBrault/StatsDCollector/Provider/SymfonyRequestProvider.php
+++ src/AdrienBrault/StatsDCollector/Provider/SymfonyRequestProvider.php
@@ -36,23 +36,29 @@ class SymfonyRequestProvider implements ParameterProviderInterface
      */
     public function getParameters()
     {
+        $method = null;
         $format = null;
         $scheme = null;
         $ajax = null;
         $locale = null;
+        $route = null;
 
         if (null !== $this->request) {
+            $method = strtolower($this->request->getMethod());
             $format = $this->request->getRequestFormat();
             $scheme = $this->request->getScheme();
             $ajax = $this->request->isXmlHttpRequest() ? 'ajax' : 'not_ajax';
             $locale = $this->request->getLocale();
+            $route = $this->request->attributes->get('_route');
         }
 
         return array(
+            'request_method' => $method,
             'request_format' => $format,
             'request_scheme' => $scheme,
             'request_ajax' => $ajax,
             'request_locale' => $locale,
+            'request_route' => $route,
         );
     }
 }
--- tests/AdrienBrault/StatsDCollector/Tests/Provider/SymfonyRequestProviderTest.php
+++ tests/AdrienBrault/StatsDCollector/Tests/Provider/SymfonyRequestProviderTest.php
@@ -4,6 +4,7 @@ namespace AdrienBrault\StatsDCollector\Tests\Provider;
 
 use AdrienBrault\StatsDCollector\Provider\SymfonyRequestProvider;
 use Hautelook\Frankenstein\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class SymfonyRequestProviderTest extends TestCase
 {
@@ -14,10 +15,12 @@ class SymfonyRequestProviderTest extends TestCase
         $this
             ->array($provider->getParameters())
                 ->isIdenticalTo(array(
+                    'request_method' => null,
                     'request_format' => null,
                     'request_scheme' => null,
                     'request_ajax' => null,
                     'request_locale' => null,
+                    'request_route' => null,
                 ))
         ;
     }
@@ -25,19 +28,23 @@ class SymfonyRequestProviderTest extends TestCase
     public function test()
     {
         $requestProphecy = $this->prophesize('Symfony\Component\HttpFoundation\Request');
+        $requestProphecy->getMethod()->willReturn('GET');
         $requestProphecy->getRequestFormat()->willReturn('json');
         $requestProphecy->getScheme()->willReturn('https');
         $requestProphecy->isXmlHttpRequest()->willReturn(false);
         $requestProphecy->getLocale()->willReturn('fr');
+        $requestProphecy->reveal()->attributes = new ParameterBag(array('_route' => 'user_get'));
         $provider = new SymfonyRequestProvider($requestProphecy->reveal());
 
         $this
             ->array($provider->getParameters())
                 ->isIdenticalTo(array(
+                    'request_method' => 'get',
                     'request_format' => 'json',
                     'request_scheme' => 'https',
                     'request_ajax' => 'not_ajax',
                     'request_locale' => 'fr',
+                    'request_route' => 'user_get',
                 ))
         ;
     }
